6e8606a8e038bfdd26af611f9b898cf0e8b6de6b
Serdar Benderli <serdar.benderli@antithesis.com>
2025-08-22 09:13:44 -0400

    add the etcd client. this is tested and works.

---
+++ etcd-k8s/client/python-generate-traffic/resources/helper.py
@@ -0,0 +1,42 @@
+import etcd3, string
+
+# Antithesis SDK
+from antithesis.random import (
+    random_choice,
+    get_random,
+)
+
+def put_request(c, key, value):
+    try:
+        c.put(key, value)
+        return True, None
+    except Exception as e:
+        return False, e
+
+def get_request(c, key):
+    try:
+        response = c.get(key)
+        database_value = response[0].decode('utf-8')
+        return True, None, database_value
+    except Exception as e:
+        return False, e, None
+
+def connect_to_host():
+    host_base = random_choice(["etcd-0", "etcd-1", "etcd-2"])
+    host = f"{host_base}.etcd-headless"
+    try:
+        client = etcd3.client(host=host, port=2379)
+        print(f"Client: connected to {host}")
+        return client
+    except Exception as e:
+        print(f"Client: failed to connect to {host}: {e}")
+        sys.exit(1)
+
+def generate_random_string():
+    random_str = []
+    for _ in range(16):
+        random_str.append(random_choice(list(string.ascii_letters + string.digits)))
+    return "".join(random_str)
+
+def generate_num_requests():
+    return (get_random() % 100) + 1
